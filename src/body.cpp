c145c115efcf80512f19009d90e6ff9ad6be711f
fo-od <fooduh@proton.me>
2025-05-15 22:12:18 -0700

    implement constructors for Body

--- src/body.cpp
+++ src/body.cpp
@@ -1,1 +1,14 @@
 #include "body.hpp"
+
+Body::Body( const AABB &aabb, const Vector2 &velocity, const Vector2 &acceleration )
+    : aabb(aabb),
+      velocity(velocity),
+      acceleration(acceleration) {}
+
+Body::Body( const AABB &aabb )
+    : aabb(aabb) {}
+
+Body::Body( const Body *other )
+    : aabb(other->aabb),
+      velocity(other->velocity),
+      acceleration(other->acceleration) {}
--- src/include/body.hpp
+++ src/include/body.hpp
@@ -5,13 +5,15 @@
 class Body
 {
     public:
-        AABB aabb;
-        Vector2 velocity;
-        Vector2 acceleration;
+        AABB aabb{};
+        Vector2 velocity{};
+        Vector2 acceleration{};
 
         Body() = default;
 
-        Body( AABB aabb, Vector2 velocity, Vector2 acceleration );
+        Body( const AABB& aabb, const Vector2& velocity, const Vector2 &acceleration );
 
-        explicit Body( Body *other );
+        explicit Body( const AABB& aabb );
+
+        explicit Body( const Body *other );
 };
