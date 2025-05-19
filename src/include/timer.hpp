70571a3bc7e4272974a05987d4fd297d12706bf5
fo-od <fooduh@proton.me>
2025-05-19 11:08:43 -0700

    add Timer: start(), stop(), pause(), resume(), get_ticks(), is_started(), is_paused()

--- src/include/timer.hpp
+++ src/include/timer.hpp
@@ -1,5 +1,35 @@
 #pragma once
+#include "types.hpp"
 
-class Timer {
+class Timer
+{
+    u64 startTicks{};
+    u64 pausedTicks{};
 
+    bool started = false;
+    bool paused = false;
+
+    public:
+        Timer() = default;
+
+        ~Timer() = default;
+
+        void start();
+
+        void stop();
+
+        void pause();
+
+        void resume();
+
+        /**
+         * @returns time since start in milliseconds
+         * @returns the time when it was paused if paused
+         * @returns 0 if not started
+         */
+        [[nodiscard]] u64 get_ticks() const;
+
+        [[nodiscard]] bool is_started() const;
+
+        [[nodiscard]] bool is_paused() const;
 };
--- src/timer.cpp
+++ src/timer.cpp
@@ -1,1 +1,51 @@
 #include "timer.hpp"
+
+#include "SDL3/SDL_timer.h"
+
+void Timer::start()
+{
+    started = true;
+    paused = false;
+    startTicks = SDL_GetTicks();
+    pausedTicks = 0;
+}
+
+void Timer::stop()
+{
+    started = false;
+    paused = false;
+    startTicks = 0;
+    pausedTicks = 0;
+}
+
+void Timer::pause()
+{
+    if ( started && !paused ) {
+        paused = true;
+        pausedTicks = SDL_GetTicks() - startTicks;
+        startTicks = 0;
+    }
+}
+
+void Timer::resume()
+{
+    if ( paused && !started ) {
+        paused = false;
+        startTicks = SDL_GetTicks() - pausedTicks;
+        pausedTicks = 0;
+    }
+}
+
+u64 Timer::get_ticks() const
+{
+    if ( started ) {
+        if ( paused ) {
+            return pausedTicks;
+        }
+        return SDL_GetTicks() - startTicks;
+    }
+    return 0;
+}
+
+bool Timer::is_started() const { return started; }
+bool Timer::is_paused() const { return paused; }
