ac714b938b20b3a4f8121609218407a18da5eb92
fo-od <fooduh@proton.me>
2025-05-18 21:40:22 -0700

    add to AABB: update_rect(), min(), max(), minkowski_difference(), intersects(), and penetration_vector

--- src/aabb.cpp
+++ src/aabb.cpp
@@ -1,5 +1,65 @@
 #include "aabb.hpp"
 
+#include <complex>
+#include <SDL3/SDL.h>
+
 AABB::AABB( const Vector2 &position, const Vector2 &half_size )
-    : position(position),
+    : rect(position.x + half_size.x, position.y - half_size.y, half_size.x * 2, half_size.y * 2),
+      position(position),
       half_size(half_size) {}
+
+
+void AABB::update_rect()
+{
+    rect.x = position.x;
+    rect.y = position.y;
+}
+
+Vector2 AABB::min() const { return position - half_size; }
+Vector2 AABB::max() const { return position + half_size; }
+
+AABB AABB::minkowski_difference( const AABB &other ) const
+{
+    return AABB{other.position - this->position, other.half_size + this->half_size};
+}
+
+bool AABB::intersects( const AABB &other ) const
+{
+    const AABB diff = minkowski_difference(other);
+    const Vector2 min = diff.min(), max = diff.max();
+
+    return ( min.x <= 0
+             && max.x >= 0
+             && min.y <= 0
+             && max.y >= 0 );
+}
+
+Vector2 AABB::penetration_vector( const AABB &other ) const
+{
+    Vector2 result;
+
+    const AABB diff = minkowski_difference(other);
+    const Vector2 min = diff.min(), max = diff.max();
+
+    float min_dist = std::abs(min.x);
+    result.x = min.x;
+    result.y = 0;
+
+    if ( std::abs(max.x) < min_dist ) {
+        min_dist = std::abs(max.x);
+        result.x = max.x;
+    }
+
+    if ( std::abs(min.y) < min_dist ) {
+        min_dist = std::abs(min.y);
+        result.x = 0;
+        result.y = min.y;
+    }
+
+    if ( std::abs(max.y) < min_dist ) {
+        result.x = 0;
+        result.y = max.y;
+    }
+
+    return result;
+}
--- src/include/aabb.hpp
+++ src/include/aabb.hpp
@@ -1,16 +1,30 @@
 #pragma once
 
+#include <SDL3/SDL_rect.h>
 #include "vector.hpp"
 
 class AABB
 {
     public:
-        Vector2 position;
-        Vector2 half_size;
+        SDL_FRect rect{};
+        Vector2 position{};
+        Vector2 half_size{};
 
         AABB() = default;
 
+        AABB( const Vector2 &position, const Vector2 &half_size );
+
         ~AABB() = default;
 
-        AABB( const Vector2 &position, const Vector2 &half_size );
+        void update_rect();
+
+        [[nodiscard]] Vector2 min() const;
+
+        [[nodiscard]] Vector2 max() const;
+
+        [[nodiscard]] AABB minkowski_difference( const AABB &other ) const;
+
+        [[nodiscard]] bool intersects( const AABB &other ) const;
+
+        [[nodiscard]] Vector2 penetration_vector( const AABB &other ) const;
 };
