e146827012a1697bf67a59928a4d516d8c02a635
fo-od <fooduh@proton.me>
2025-05-21 12:00:31 -0700

    add rects back to physics because it just wont work if i try to put it in AABB

--- src/include/physics.hpp
+++ src/include/physics.hpp
@@ -7,11 +7,12 @@
 class Physics
 {
     public:
+        static std::vector< SDL_FRect * > rects;
         static std::vector< Body > bodies;
 
         static void update();
 
         static void add_body( const Vector2 &position, const Vector2 &size,
-                              const Vector2 &velocity = Vector2(0, 0),
-                              const Vector2 &acceleration = Vector2(0, 0) );
+                              const Vector2 &velocity = Vector2{},
+                              const Vector2 &acceleration = Vector2{} );
 };
