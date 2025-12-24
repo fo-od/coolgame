5e016228f8554427723a0646c1a96b665bba8ee9
fo-od <fooduh@proton.me>
2025-12-23 22:08:21 -0800

    fix OOR error when trying to close 2 menus at the same time

--- src/include/util.hpp
+++ src/include/util.hpp
@@ -20,12 +20,12 @@
 #define ANCHOR_TOP 8
 #define ANCHOR_BOTTOM 16
 
-#define OPEN_MENU(menu) Menu::open(menu); mInMenu=true; mCurrentMenu=menu;
-#define CLOSE_MENU(menu) Menu::close(menu); mInMenu=false; mCurrentMenu="";
+#define OPEN_MENU(menu) {Menu::open(menu); mInMenu=true; mCurrentMenu=menu;}
+#define CLOSE_MENU(menu) if (mCurrentMenu!="") {Menu::close(menu); mInMenu=false; mCurrentMenu="";}
 
 void U_DrawRendererText( TTF_Text *text, float x, float y, int anchor = ANCHOR_TOP | ANCHOR_LEFT );
 
-Vector2 U_GetTextSize(TTF_Text * text);
+Vector2 U_GetTextSize( TTF_Text *text );
 
 void U_AnchorFRect( SDL_FRect *rect, int anchor );
 
