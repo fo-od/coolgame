0f0b0c24403666a85f113cfc3daa2d52a649b20d
fo-od <fooduh@proton.me>
2026-03-31 16:05:12 -0700

    last commit before i archive this probably

--- src/main.cpp
+++ src/main.cpp
@@ -9,235 +9,238 @@
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
-struct SDLApplication
+static SDL_Window *mWindow{};
+static SDL_Renderer *mRenderer{};
+static TTF_Font *mFont;
+static TTF_TextEngine *mTextEngine;
+
+static const bool *keyboardState{};
+static std::string mCurrentMenu;
+
+static float deltaTime = 0.0f;
+static float camX = 0.0f;
+static float camY = 0.0f;
+
+static bool mRunning = false;
+static GameState mGameState = GameState::InMenu;
+
+static int mWidth = 640;
+static int mHeight = 480;
+
+static void init()
 {
-    SDL_Window *mWindow{};
-    SDL_Renderer *mRenderer{};
-    TTF_Font *mFont;
-    TTF_TextEngine *mTextEngine;
+    // sdl setup stuff
+    SDL_SetAppMetadata("cool game", "0.1", "com.food.coolgame");
+    SDL_Init(SDL_INIT_VIDEO);
 
-    const bool *keyboardState{};
-    std::string mCurrentMenu;
+    mWindow = SDL_CreateWindow("cool game", mWidth, mHeight, SDL_WINDOW_RESIZABLE |
+                                                             SDL_WINDOW_BORDERLESS);
+    mRenderer = SDL_CreateRenderer(mWindow, nullptr);
 
-    float deltaTime = 0.0f;
-    float camX = 0.0f;
-    float camY = 0.0f;
+    // enable vsync
+    SDL_SetRenderVSync(mRenderer, 1);
 
-    bool mRunning = false;
-    GameState mGameState = GameState::InMenu;
+    // blend alpha channel
+    SDL_SetRenderDrawBlendMode(mRenderer, SDL_BLENDMODE_BLEND);
 
-    int mWidth = 640;
-    int mHeight = 480;
+    TTF_Init();
+    mTextEngine = TTF_CreateRendererTextEngine(mRenderer);
 
-    SDLApplication()
+    const std::filesystem::path basePath = SDL_GetBasePath();
+    const auto fontPath = basePath / "assets/cozette.fnt";
+    mFont = TTF_OpenFont(fontPath.string().c_str(), 13);
+
+    mRunning = true;
+
+    OPEN_MENU("mainMenu")
+}
+
+static void exit()
+{
+    TTF_CloseFont(mFont);
+    TTF_Quit();
+
+    SDL_DestroyRenderer(mRenderer);
+    SDL_DestroyWindow(mWindow);
+    SDL_Quit();
+}
+
+static void input( SDL_Event *e )
+{
+    if ( e->type == SDL_EVENT_QUIT )
     {
-        // sdl setup stuff
-        SDL_SetAppMetadata("cool game", "0.1", "com.food.coolgame");
-        SDL_Init(SDL_INIT_VIDEO);
-
-        mWindow = SDL_CreateWindow("cool game", mWidth, mHeight, SDL_WINDOW_RESIZABLE |
-                                                                 SDL_WINDOW_BORDERLESS);
-        mRenderer = SDL_CreateRenderer(mWindow, nullptr);
-
-        // enable vsync
-        SDL_SetRenderVSync(mRenderer, 1);
-
-        // blend alpha channel
-        SDL_SetRenderDrawBlendMode(mRenderer, SDL_BLENDMODE_BLEND);
-
-        TTF_Init();
-        mTextEngine = TTF_CreateRendererTextEngine(mRenderer);
-
-        const std::filesystem::path basePath = SDL_GetBasePath();
-        const auto fontPath = basePath / "assets/cozette.fnt";
-        mFont = TTF_OpenFont(fontPath.string().c_str(), 13);
-
-        //menus
-        Menu::create("pauseMenu", Element::make_vector(
-                                                       std::make_unique< Button >(mTextEngine, mFont,
-                                                                0, 20,
-                                                                Anchor::Center, Anchor::Center,
-                                                                "Resume",
-                                                                [this]
-                                                                {
-                                                                    CLOSE_MENU(mCurrentMenu)
-                                                                }),
-                                                       std::make_unique< Button >(mTextEngine, mFont,
-                                                                0, -20,
-                                                                Anchor::Center, Anchor::Center,
-                                                                "Exit",
-                                                                [this]
-                                                                {
-                                                                    CLOSE_MENU(mCurrentMenu)
-                                                                    mRunning = false;
-                                                                })
-                                                      ));
-
-        Menu::create("mainMenu", Element::make_vector(
-                                                      std::make_unique< Button >(mTextEngine, mFont, 0, 20,
-                                                               Anchor::Center, Anchor::Center,
-                                                               "Levels",
-                                                               [this]
-                                                               {
-                                                                   CLOSE_MENU(mCurrentMenu)
-                                                                   OPEN_MENU("pauseMenu")
-                                                                   // todo: make a level select menu
-                                                                   mGameState = mGameState | GameState::Playing;
-                                                               }),
-                                                      std::make_unique< Button >(mTextEngine, mFont, 0, -20,
-                                                               Anchor::Center, Anchor::Center,
-                                                               "Create",
-                                                               [this]
-                                                               {
-                                                                   mGameState = mGameState | GameState::Editing;
-
-                                                                   CLOSE_MENU(mCurrentMenu)
-                                                               })
-                                                     ));
+        mRunning = false;
     }
 
-    ~SDLApplication()
+    if ( e->type == SDL_EVENT_WINDOW_RESIZED )
     {
-        TTF_CloseFont(mFont);
-        TTF_Quit();
-
-        SDL_DestroyRenderer(mRenderer);
-        SDL_DestroyWindow(mWindow);
-        SDL_Quit();
+        SDL_GetWindowSizeInPixels(mWindow, &mWidth, &mHeight);
+        Menu::update(mWidth, mHeight);
     }
 
-    void mainLoop()
+    keyboardState = SDL_GetKeyboardState(nullptr);
+
+    if ( e->type == SDL_EVENT_KEY_DOWN )
     {
-        mRunning = true;
-
-        OPEN_MENU("mainMenu")
-
-        const u32 player = Physics::add_body(Vector2{mWidth / 2, mHeight / 2}, Vector2{10, 10}, true);
-
-        const u32 floor = Physics::add_static_body(Vector2{mWidth / 2, mHeight - 25},
-                                                   Vector2{mWidth / 2, 25});
-        const u32 left_wall = Physics::add_static_body(Vector2{25, mHeight / 2},
-                                                       Vector2{25, mHeight / 2});
-        const u32 right_wall = Physics::add_static_body(Vector2{mWidth - 25, mHeight / 2},
-                                                        Vector2{25, mHeight / 2});
-        const u32 ceiling = Physics::add_static_body(Vector2{mWidth / 2, 25}, Vector2{mWidth / 2, 25});
-
-        while ( mRunning )
+        if ( e->key.key == SDLK_ESCAPE )
         {
-            // todo: let player add and edit platforms (in game loop)
-            const u64 currentTick = SDL_GetTicks();
-
-            SDL_Event event;
-            while ( SDL_PollEvent(&event) )
-            {
-                input(&event);
-            }
-
             if ( !(mGameState & GameState::InMenu) )
             {
-                update(Physics::get_body(player));
-            }
-
-            render();
-
-            deltaTime = static_cast< float >(SDL_GetTicks() - currentTick) / 1000.0f;
-        }
-    }
-
-    void input( SDL_Event *e )
-    {
-        if ( e->type == SDL_EVENT_QUIT )
-        {
-            mRunning = false;
-        }
-
-        if ( e->type == SDL_EVENT_WINDOW_RESIZED )
-        {
-            SDL_GetWindowSizeInPixels(mWindow, &mWidth, &mHeight);
-            Menu::update(mWidth, mHeight);
-        }
-
-        keyboardState = SDL_GetKeyboardState(nullptr);
-
-        if ( e->type == SDL_EVENT_KEY_DOWN )
-        {
-            if ( e->key.key == SDLK_ESCAPE )
+                OPEN_MENU("pauseMenu")
+            } else
             {
-                if ( !(mGameState & GameState::InMenu) )
-                {
-                    OPEN_MENU("pauseMenu")
-                } else
-                {
-                    CLOSE_MENU(mCurrentMenu)
-                }
-            }
-        }
-
-        if ( e->type == SDL_EVENT_MOUSE_MOTION ||
-             e->type == SDL_EVENT_MOUSE_BUTTON_DOWN ||
-             e->type == SDL_EVENT_MOUSE_BUTTON_UP )
-        {
-            SDL_ConvertEventToRenderCoordinates(mRenderer, e);
-
-            if ( mGameState & GameState::InMenu )
-            {
-                Menu::handle_input(e->motion, e->button);
+                CLOSE_MENU(mCurrentMenu)
             }
         }
     }
 
-    void update( Body *player ) const
+    if ( e->type == SDL_EVENT_MOUSE_MOTION ||
+         e->type == SDL_EVENT_MOUSE_BUTTON_DOWN ||
+         e->type == SDL_EVENT_MOUSE_BUTTON_UP )
     {
-        Vector2 velocity{0.0, player->mVelocity.y};
-
-        if ( keyboardState[SDL_SCANCODE_LEFT] )
-        {
-            velocity.x -= 1000;
-        }
-        if ( keyboardState[SDL_SCANCODE_RIGHT] )
-        {
-            velocity.x += 1000;
-        }
-        if ( keyboardState[SDL_SCANCODE_UP] )
-        {
-            velocity.y = -2000;
-        }
-        if ( keyboardState[SDL_SCANCODE_DOWN] )
-        {
-            velocity.y += 800;
-        }
-
-        player->mVelocity = velocity;
-
-        Physics::update(deltaTime);
-    }
-
-    void render() const
-    {
-        SDL_SetRenderDrawColor(mRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        SDL_RenderClear(mRenderer);
+        SDL_ConvertEventToRenderCoordinates(mRenderer, e);
 
         if ( mGameState & GameState::InMenu )
         {
-            // draw the world in a darker color for better menu visibility
-            SDL_SetRenderDrawColor(mRenderer, 127, 127, 127, SDL_ALPHA_OPAQUE);
-            AABB::draw(mRenderer);
-
-            SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            Menu::draw(mRenderer);
-        } else
-        {
-            SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            AABB::draw(mRenderer);
+            Menu::handle_input(e->motion, e->button);
         }
-
-        SDL_RenderPresent(mRenderer);
     }
-};
+}
+
+static void update( Body *player )
+{
+    Vector2 velocity{0.0, player->mVelocity.y};
+
+    if ( keyboardState[SDL_SCANCODE_LEFT] )
+    {
+        velocity.x -= 1000;
+    }
+    if ( keyboardState[SDL_SCANCODE_RIGHT] )
+    {
+        velocity.x += 1000;
+    }
+    if ( keyboardState[SDL_SCANCODE_UP] )
+    {
+        velocity.y = -2000;
+    }
+    if ( keyboardState[SDL_SCANCODE_DOWN] )
+    {
+        velocity.y += 800;
+    }
+
+    player->mVelocity = velocity;
+
+    Physics::update(deltaTime);
+}
+
+static void render()
+{
+    SDL_SetRenderDrawColor(mRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(mRenderer);
+
+    if ( mGameState & GameState::InMenu )
+    {
+        // draw the world in a darker color for better menu visibility
+        SDL_SetRenderDrawColor(mRenderer, 127, 127, 127, SDL_ALPHA_OPAQUE);
+        AABB::draw(mRenderer);
+
+        SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        Menu::draw(mRenderer);
+    } else
+    {
+        SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        AABB::draw(mRenderer);
+    }
+
+    SDL_RenderPresent(mRenderer);
+}
+
+static void createMenu() {}
+
+static void create_menus()
+{
+    Menu::create("pauseMenu", Element::make_vector(
+                                                   std::make_unique< Button >(mTextEngine, mFont,
+                                                                              0, 20,
+                                                                              Anchor::Center, Anchor::Center,
+                                                                              "Resume",
+                                                                              []
+                                                                              {
+                                                                                  CLOSE_MENU(mCurrentMenu)
+                                                                              }),
+                                                   std::make_unique< Button >(mTextEngine, mFont,
+                                                                              0, -20,
+                                                                              Anchor::Center, Anchor::Center,
+                                                                              "Exit",
+                                                                              []
+                                                                              {
+                                                                                  CLOSE_MENU(mCurrentMenu)
+                                                                                  mRunning = false;
+                                                                              })
+                                                  ));
+
+    Menu::create("mainMenu", Element::make_vector(
+                                                  std::make_unique< Button >(mTextEngine, mFont, 0, 20,
+                                                                             Anchor::Center, Anchor::Center,
+                                                                             "Levels",
+                                                                             []
+                                                                             {
+                                                                                 CLOSE_MENU(mCurrentMenu)
+                                                                                 OPEN_MENU("pauseMenu")
+                                                                                 // todo: make a level select menu
+                                                                                 mGameState =
+                                                                                     mGameState | GameState::Playing;
+                                                                             }),
+                                                  std::make_unique< Button >(mTextEngine, mFont, 0, -20,
+                                                                             Anchor::Center, Anchor::Center,
+                                                                             "Create",
+                                                                             []
+                                                                             {
+                                                                                 mGameState =
+                                                                                     mGameState | GameState::Editing;
+                                                                                 createMenu();
+                                                                                 CLOSE_MENU(mCurrentMenu)
+                                                                             })
+                                                 ));
+}
 
 int main()
 {
-    SDLApplication app;
-    app.mainLoop();
+    init();
+    create_menus();
+
+    const u32 player = Physics::add_body(Vector2{mWidth / 2, mHeight / 2}, Vector2{10, 10}, true);
+
+    const u32 floor = Physics::add_static_body(Vector2{mWidth / 2, mHeight - 25},
+                                               Vector2{mWidth / 2, 25});
+    const u32 left_wall = Physics::add_static_body(Vector2{25, mHeight / 2},
+                                                   Vector2{25, mHeight / 2});
+    const u32 right_wall = Physics::add_static_body(Vector2{mWidth - 25, mHeight / 2},
+                                                    Vector2{25, mHeight / 2});
+    const u32 ceiling = Physics::add_static_body(Vector2{mWidth / 2, 25}, Vector2{mWidth / 2, 25});
+
+    while ( mRunning )
+    {
+        // todo: let player add and edit platforms (in game loop)
+        const u64 currentTick = SDL_GetTicks();
+
+        SDL_Event event;
+        while ( SDL_PollEvent(&event) )
+        {
+            input(&event);
+        }
+
+        if ( !(mGameState & GameState::InMenu) )
+        {
+            update(Physics::get_body(player));
+        }
+
+        render();
+
+        deltaTime = (SDL_GetTicks() - currentTick) / 1000.0f;
+    }
+
+    exit();
+
     return 0;
 }
